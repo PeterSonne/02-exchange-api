d27ab6311dcbc33931525453fccf58bdd114a340
Sören Peterson <papa@deep2chill.de>
2020-02-10 04:03:32 -0500

    Added Parameter

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ app.get("/", (req, res) => {
   res.send("Good Morning");
 });
 
-app.get("/currency", (req, res, amt) => {
+app.get("/currency", (req, res) => {
   // tmp
-  amt = 101;
-  const targetCurrency = "AUD";
-  const baseCurrency = "EUR";
-
+  const amt = req.query.amt ? req.query.amt : 100;
+  const targetCurrency =
+    req.query.sym && typeof req.query.sym == "string" ? req.query.sym : "AUD";
+  const baseCurrency =
+    req.query.base && typeof req.query.base == "string"
+      ? req.query.base
+      : "EUR";
+  //   console.log(req.query);
   axios
     .get(
       `https://api.exchangeratesapi.io/latest?base=${baseCurrency}&symbols=${targetCurrency}`
